82b571e40ac0826b38ce40179ce244e6dbb0a4fa
pparmar1 <pparmar1@binghamton.edu>
2018-03-30 19:27:31 -0400

    creating enum

---
+++ campusTour/src/studentOrientation/util/ActivitiesEnum.java
@@ -0,0 +1,12 @@
+package studentOrientation.util;
+
+public enum ActivitiesEnum {
+    SOM,
+    WATSON,
+    CS240,
+    CS350,
+    EVENTCENTER,
+    UNIVERSITYUNION,
+    CIW,
+    MOUNTAINVIEW;
+}
