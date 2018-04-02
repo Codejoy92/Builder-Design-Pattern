2c6119355b3249d8f510a30980dc3643764e8180
Sujoy Das <sdas4@binghamton.edu>
2018-04-02 16:47:25 -0400

    changed enum and arguments

--- campusTour/src/studentOrientation/driver/Driver.java
+++ campusTour/src/studentOrientation/driver/Driver.java
@@ -8,7 +8,7 @@ import studentOrientation.util.SchedulerWorkshop;
 
 public class Driver {
     public static void main(String[] args) {
-        ScheduleI createSchedule = new Schedule(ActivitiesEnum.CIW, ActivitiesEnum.CS240, ActivitiesEnum.UNIVERSITYUNION,ActivitiesEnum.WATSON);
+        ScheduleI createSchedule = new Schedule(ActivitiesEnum.CIW_BUS, ActivitiesEnum.CS240_BUS, ActivitiesEnum.UNIVERSITYUNION_BUS,ActivitiesEnum.WATSON_BUS);
         WorkshopI schedule = new SchedulerWorkshop();
         schedule.construct(createSchedule);
     }
--- campusTour/src/studentOrientation/util/Schedule.java
+++ campusTour/src/studentOrientation/util/Schedule.java
@@ -31,13 +31,13 @@ public class Schedule implements ScheduleI {
 	public Schedule(ActivitiesEnum value1, ActivitiesEnum value2, ActivitiesEnum value3, ActivitiesEnum value4) {
 
 		// Checking for value1
-		if (value1 == ActivitiesEnum.CIW || value1 == ActivitiesEnum.MOUNTAINVIEW) {
+		if (value1 == ActivitiesEnum.CIW_BUS || value1 == ActivitiesEnum.CIW_FOOT || value1 == ActivitiesEnum.MOUNTAINVIEW_BUS || value1 == ActivitiesEnum.MOUNTAINVIEW_FOOT) {
 			setCafeteria(value1);
-		} else if (value1 == ActivitiesEnum.UNIVERSITYUNION || value1 == ActivitiesEnum.EVENTCENTER) {
+		} else if (value1 == ActivitiesEnum.UNIVERSITYUNION_BUS || value1 == ActivitiesEnum.UNIVERSITYUNION_FOOT || value1 == ActivitiesEnum.EVENTCENTER_BUS || value1 == ActivitiesEnum.EVENTCENTER_FOOT) {
 			setGift(value1);
-		} else if (value1 == ActivitiesEnum.SOM || value1 == ActivitiesEnum.WATSON) {
+		} else if (value1 == ActivitiesEnum.SOM_BUS || value1 == ActivitiesEnum.SOM_FOOT || value1 == ActivitiesEnum.WATSON_BUS || value1 == ActivitiesEnum.WATSON_FOOT) {
 			setBuilding(value1);
-		} else if (value1 == ActivitiesEnum.CS240 || value1 == ActivitiesEnum.CS350) {
+		} else if (value1 == ActivitiesEnum.CS240_BUS || value1 == ActivitiesEnum.CS240_FOOT || value1 == ActivitiesEnum.CS350_BUS || value1 == ActivitiesEnum.CS350_FOOT) {
 			setLecture(value1);
 		} else {
 			System.out.println("invalid arugument 1");
@@ -45,44 +45,44 @@ public class Schedule implements ScheduleI {
 		}
 
 		// checking for value2
-		if (value2 == ActivitiesEnum.CIW || value2 == ActivitiesEnum.MOUNTAINVIEW) {
+		if (value2 == ActivitiesEnum.CIW_BUS || value2 == ActivitiesEnum.CIW_FOOT || value2 == ActivitiesEnum.MOUNTAINVIEW_BUS || value2 == ActivitiesEnum.MOUNTAINVIEW_FOOT) {
 			setCafeteria(value2);
-		} else if (value2 == ActivitiesEnum.UNIVERSITYUNION || value2 == ActivitiesEnum.EVENTCENTER) {
+		} else if (value2 == ActivitiesEnum.UNIVERSITYUNION_BUS || value2 == ActivitiesEnum.UNIVERSITYUNION_FOOT || value2 == ActivitiesEnum.EVENTCENTER_BUS || value2 == ActivitiesEnum.EVENTCENTER_FOOT) {
 			setGift(value2);
-		} else if (value2 == ActivitiesEnum.SOM || value2 == ActivitiesEnum.WATSON) {
+		} else if (value2 == ActivitiesEnum.SOM_BUS || value2 == ActivitiesEnum.SOM_FOOT || value2 == ActivitiesEnum.WATSON_BUS || value2 == ActivitiesEnum.WATSON_FOOT) {
 			setBuilding(value2);
-		} else if (value2 == ActivitiesEnum.CS240 || value2 == ActivitiesEnum.CS350) {
+		} else if (value2 == ActivitiesEnum.CS240_BUS || value2 == ActivitiesEnum.CS240_FOOT || value2 == ActivitiesEnum.CS350_BUS || value2 == ActivitiesEnum.CS350_FOOT) {
 			setLecture(value2);
 		} else {
-			System.out.println("invalid arugument 2");
+			System.out.println("invalid arugument 1");
 			System.exit(0);
 		}
 
 		// checking for value3
-		if (value3 == ActivitiesEnum.CIW || value3 == ActivitiesEnum.MOUNTAINVIEW) {
+		if (value3 == ActivitiesEnum.CIW_BUS || value3 == ActivitiesEnum.CIW_FOOT || value3 == ActivitiesEnum.MOUNTAINVIEW_BUS || value3 == ActivitiesEnum.MOUNTAINVIEW_FOOT) {
 			setCafeteria(value3);
-		} else if (value3 == ActivitiesEnum.UNIVERSITYUNION || value3 == ActivitiesEnum.EVENTCENTER) {
+		} else if (value3 == ActivitiesEnum.UNIVERSITYUNION_BUS || value3 == ActivitiesEnum.UNIVERSITYUNION_FOOT || value3 == ActivitiesEnum.EVENTCENTER_BUS || value3 == ActivitiesEnum.EVENTCENTER_FOOT) {
 			setGift(value3);
-		} else if (value3 == ActivitiesEnum.SOM || value3 == ActivitiesEnum.WATSON) {
+		} else if (value3 == ActivitiesEnum.SOM_BUS || value3 == ActivitiesEnum.SOM_FOOT || value3 == ActivitiesEnum.WATSON_BUS || value3 == ActivitiesEnum.WATSON_FOOT) {
 			setBuilding(value3);
-		} else if (value3 == ActivitiesEnum.CS240 || value3 == ActivitiesEnum.CS350) {
+		} else if (value3 == ActivitiesEnum.CS240_BUS || value3 == ActivitiesEnum.CS240_FOOT || value3 == ActivitiesEnum.CS350_BUS || value3 == ActivitiesEnum.CS350_FOOT) {
 			setLecture(value3);
 		} else {
-			System.out.println("invalid arugument 3");
+			System.out.println("invalid arugument 1");
 			System.exit(0);
 		}
 
 		// checking for value4
-		if (value4 == ActivitiesEnum.CIW || value4 == ActivitiesEnum.MOUNTAINVIEW) {
+		if (value4 == ActivitiesEnum.CIW_BUS || value4 == ActivitiesEnum.CIW_FOOT || value4 == ActivitiesEnum.MOUNTAINVIEW_BUS || value4 == ActivitiesEnum.MOUNTAINVIEW_FOOT) {
 			setCafeteria(value4);
-		} else if (value4 == ActivitiesEnum.UNIVERSITYUNION || value4 == ActivitiesEnum.EVENTCENTER) {
+		} else if (value4 == ActivitiesEnum.UNIVERSITYUNION_BUS || value4 == ActivitiesEnum.UNIVERSITYUNION_FOOT || value4 == ActivitiesEnum.EVENTCENTER_BUS || value4 == ActivitiesEnum.EVENTCENTER_FOOT) {
 			setGift(value4);
-		} else if (value4 == ActivitiesEnum.SOM || value4 == ActivitiesEnum.WATSON) {
+		} else if (value4 == ActivitiesEnum.SOM_BUS || value4 == ActivitiesEnum.SOM_FOOT || value4 == ActivitiesEnum.WATSON_BUS || value4 == ActivitiesEnum.WATSON_FOOT) {
 			setBuilding(value4);
-		} else if (value4 == ActivitiesEnum.CS240 || value4 == ActivitiesEnum.CS350) {
+		} else if (value4 == ActivitiesEnum.CS240_BUS || value4 == ActivitiesEnum.CS240_FOOT || value4 == ActivitiesEnum.CS350_BUS || value4 == ActivitiesEnum.CS350_FOOT) {
 			setLecture(value4);
 		} else {
-			System.out.println("invalid arugument 4");
+			System.out.println("invalid arugument 1");
 			System.exit(0);
 		}
 
